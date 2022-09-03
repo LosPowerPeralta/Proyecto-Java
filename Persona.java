49a4e78bb75f1061c4f10f76ff8eb5b342e970d2
JosephDonoso <86876275+JosephDonoso@users.noreply.github.com>
2022-09-02 20:32:45 -0400

    Estética y sobrecarga

--- Persona.java
+++ Persona.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Persona {
 	private String nombre;
-	//private int numero;
+	private String numero;
 	private int rut;
 	private ArrayList< OrdenDeTrabajo > ordenesDeTrabajo;
 	
@@ -12,178 +12,160 @@ public class Persona {
 	public Persona(String nombre, int rut ) {
 		this.nombre = nombre;
 		this.rut = rut;
+		numero = null;
+		ordenesDeTrabajo = new ArrayList< OrdenDeTrabajo >();
+	}
+	
+	public Persona(String nombre, String numero, int rut ) {
+		this.nombre = nombre;
+		this.rut = rut;
+		this.numero = numero;
 		ordenesDeTrabajo = new ArrayList< OrdenDeTrabajo >();
 	}
 	
 	public void listarOrdenes() {
 		OrdenDeTrabajo ordenAuxiliar = new OrdenDeTrabajo();
-		Persona empleadoAuxiliar = new Persona();
-		Persona clienteAuxiliar = new Persona();
 		
 		if(ordenesDeTrabajo.size() == 0) {
 			System.out.println("No se registra ninguna orden en nuestra base de datos.");
-			System.out.println("Volviendo al menú");
+			System.out.println("Volviendo al menú\n");
 			return;
 		}
 		
 		for(int i = 0; i < ordenesDeTrabajo.size(); i++) {
 			ordenAuxiliar = ordenesDeTrabajo.get(i);
-			empleadoAuxiliar = ordenAuxiliar.getEmpleado();
-			clienteAuxiliar = ordenAuxiliar.getCliente();
+			ordenAuxiliar.mostrarOrdenParaCliente();
 			
-			System.out.println("	\nORDEN DE TRABAJO N°: " + ordenAuxiliar.getId());
-			System.out.println("	DATOS DEL EQUIPO: " + ordenAuxiliar.getNombreProducto());
-			System.out.println("	DIAGNÓSTICO TÉCNICO: " + ordenAuxiliar.getDiagnostico());
-			System.out.println("	\nEMPLEADO: " + empleadoAuxiliar.getNombre());
-			System.out.println("	RUT N°: " + empleadoAuxiliar.getRut());
-			System.out.println("	\nCLIENTE: " + clienteAuxiliar.getNombre());
-			System.out.println("	RUT N°: " + clienteAuxiliar.getRut());
-			//System.out.println("	FECHA DE ENTREGA: ");
-			System.out.println("	\nTOTAL: $" + ordenAuxiliar.getTotal());
-			if(ordenAuxiliar.getCancelada()) {
-				System.out.println("	ORDEN CANCELADA");
-			}
 		}
 	}
 	
 	public void buscarOrdenEmpleado(int idOrden){
 		OrdenDeTrabajo ordenAuxiliar = new OrdenDeTrabajo();
-		Persona empleadoAuxiliar = new Persona();
 		
 		if(ordenesDeTrabajo.size() == 0) {
 			System.out.println("No se registra ninguna orden en nuestra base de datos.");
-			System.out.println("Volviendo al menú");
+			System.out.println("Volviendo al menú\n");
 			return;
 		}
 		
 		for(int i = 0; i < ordenesDeTrabajo.size(); i++) {
 			ordenAuxiliar = ordenesDeTrabajo.get(i);
 			if(ordenAuxiliar.getId() == idOrden){
-				empleadoAuxiliar = ordenAuxiliar.getEmpleado();
-				
-				System.out.println("	\nORDEN DE TRABAJO N°: " + ordenAuxiliar.getId());
-				System.out.println("	NOMBRE DEL PRODUCTO A REPARAR: " + ordenAuxiliar.getNombreProducto());
-				System.out.println("	DIAGNÓSTICO TÉCNICO: " + ordenAuxiliar.getDiagnostico());
-				System.out.println("	\nEMPLEADO: " + empleadoAuxiliar.getNombre());
-				System.out.println("	RUT N°: " + empleadoAuxiliar.getRut());
-				//System.out.println("	COSTO DE REPUESTO: ");
-				//System.out.println("	APLAZAMIENTO DE FECHA POR REPUESTOS: ");
-				//System.out.println("	FECHA DE ENTREGA: ");
-				System.out.println("	TOTAL: $" + ordenAuxiliar.getTotal());
-				if(ordenAuxiliar.getCancelada()) {
-					System.out.println("	ORDEN CANCELADA");
-				}
+				ordenAuxiliar.mostrarOrdenParaEmpleado();
 				return;
 			}
 			
 		}
 		
-		System.out.println("No se ha encontrado ninguna orden con ese id");
+		System.out.println("No se ha encontrado ninguna orden con ese id\n");
+	}
+	
+	public void buscarOrdenEmpleado(double idOrden){
+		buscarOrdenCliente((int) idOrden);
+	}
+	
+	public void buscarOrdenEmpleado(long idOrden){
+		buscarOrdenCliente((int) idOrden);
 	}
 	
 	public void buscarOrdenCliente(int idOrden){
 		OrdenDeTrabajo ordenAuxiliar = new OrdenDeTrabajo();
-		Persona empleadoAuxiliar = new Persona();
 		
 		if(ordenesDeTrabajo.size() == 0) {
 			System.out.println("No se registra ninguna orden en nuestra base de datos.");
-			System.out.println("Volviendo al menú");
+			System.out.println("Volviendo al menú\n");
 			return;
 		}
 		
 		for(int i = 0; i < ordenesDeTrabajo.size(); i++) {
 			ordenAuxiliar = ordenesDeTrabajo.get(i);
 			if(ordenAuxiliar.getId() == idOrden){
-				empleadoAuxiliar = ordenAuxiliar.getEmpleado();
-				
-				System.out.println("	\nORDEN DE TRABAJO N°: " + ordenAuxiliar.getId());
-				System.out.println("	NOMBRE DEL PRODUCTO A REPARAR: " + ordenAuxiliar.getNombreProducto());
-				System.out.println("	DIAGNÓSTICO TÉCNICO: " + ordenAuxiliar.getDiagnostico());
-				System.out.println("	\nEMPLEADO: " + empleadoAuxiliar.getNombre());
-				System.out.println("	TOTAL: $" + ordenAuxiliar.getTotal());
-				if(ordenAuxiliar.getCancelada()) {
-					System.out.println("	ORDEN CANCELADA");
-				}
+				ordenAuxiliar.mostrarOrdenParaCliente();
 				return;
 			}
 			
 		}
 		
-		System.out.println("No se ha encontrado ninguna orden con ese id");
+		System.out.println("No se ha encontrado ninguna orden con ese id\n");
+	}
+	
+	public void buscarOrdenCliente(double idOrden){
+		buscarOrdenCliente((int) idOrden);
+	}
+	
+	public void buscarOrdenCliente(long idOrden){
+		buscarOrdenCliente((int) idOrden);
 	}
 	
 	public void cancelarOrden(int idOrden) {
 		OrdenDeTrabajo ordenAuxiliar = new OrdenDeTrabajo();
-		Persona empleadoAuxiliar = new Persona();
 		
 		if(ordenesDeTrabajo.size() == 0) {
 			System.out.println("No se registra ninguna orden en nuestra base de datos.");
-			System.out.println("Volviendo al menú");
+			System.out.println("Volviendo al menú\n");
 			return;
 		}
 		
 		for(int i = 0; i < ordenesDeTrabajo.size(); i++) {
 			ordenAuxiliar = ordenesDeTrabajo.get(i);
 			if(ordenAuxiliar.getId() == idOrden){
-				empleadoAuxiliar = ordenAuxiliar.getEmpleado();
-				
-				System.out.println("	\nORDEN DE TRABAJO N°: " + ordenAuxiliar.getId());
-				System.out.println("	NOMBRE DEL PRODUCTO A REPARAR: " + ordenAuxiliar.getNombreProducto());
-				System.out.println("	DIAGNÓSTICO TÉCNICO: " + ordenAuxiliar.getDiagnostico());
-				System.out.println("	\nEMPLEADO: " + empleadoAuxiliar.getNombre());
-				System.out.println("	RUT N°: " + empleadoAuxiliar.getRut());
-				//System.out.println("	COSTO DE REPUESTO: ");
-				//System.out.println("	APLAZAMIENTO DE FECHA POR REPUESTOS: ");
-				//System.out.println("	FECHA DE ENTREGA: ");
-				System.out.println("	TOTAL: $" + ordenAuxiliar.getTotal());
-				System.out.println("	ORDEN CANCELADA");
-				if(ordenAuxiliar.getCancelada()) {
-				}
-				else {
+				if(!ordenAuxiliar.getCancelada()) {
 					ordenAuxiliar.setCancelada(true);
 				}
+				ordenAuxiliar.mostrarOrdenParaCliente();
 				return;
 			}
-			
 		}
 		
-		System.out.println("No se ha encontrado ninguna orden con ese id");
+		System.out.println("No se ha encontrado ninguna orden con ese id\n");
 	}
 	
 	public void actualizarOrdenes() {
 		OrdenDeTrabajo ordenAuxiliar = new OrdenDeTrabajo();
-		Persona empleadoAuxiliar = new Persona();
-		Persona clienteAuxiliar = new Persona();
 		
 		if(ordenesDeTrabajo.size() == 0) {
 			System.out.println("No se registra ninguna orden en nuestra base de datos.");
-			System.out.println("Volviendo al menú");
+			System.out.println("Volviendo al menú\n");
 			return;
 		}
 		
 		for(int i = 0; i < ordenesDeTrabajo.size(); i++) {
 			ordenAuxiliar = ordenesDeTrabajo.get(i);
-			empleadoAuxiliar = ordenAuxiliar.getEmpleado();
-			clienteAuxiliar = ordenAuxiliar.getCliente();
+			
 			if(ordenAuxiliar.getCancelada()) {
-				System.out.println("	\nORDEN DE TRABAJO N°: " + ordenAuxiliar.getId());
-				System.out.println("	DATOS DEL EQUIPO: " + ordenAuxiliar.getNombreProducto());
-				System.out.println("	DIAGNÓSTICO TÉCNICO: " + ordenAuxiliar.getDiagnostico());
-				System.out.println("	\nEMPLEADO: " + empleadoAuxiliar.getNombre());
-				System.out.println("	RUT N°: " + empleadoAuxiliar.getRut());
-				System.out.println("	\nCLIENTE: " + clienteAuxiliar.getNombre());
-				System.out.println("	RUT N°: " + clienteAuxiliar.getRut());
-				//System.out.println("	FECHA DE ENTREGA: ");
-				System.out.println("	\nTOTAL: $" + ordenAuxiliar.getTotal());
-				System.out.println("	LA ORDEN FUE CANCELADA");
-				
+				ordenAuxiliar.mostrarOrdenParaCliente();
 				ordenesDeTrabajo.remove(i);
 			}
 		}
 	}
 	
+	public void agregarNuevaOrden(OrdenDeTrabajo nuevaOrden, Persona empleado) {
+		ordenesDeTrabajo.add(nuevaOrden);
+		empleado.ordenesDeTrabajo.add(nuevaOrden);
+		
+	}
+	
 	public void agregarNuevaOrden(OrdenDeTrabajo nuevaOrden) {
 		ordenesDeTrabajo.add(nuevaOrden);
+	}
+	
+	public void agregarNuevaOrden(String nombreProducto, String diagnostico, Persona cliente, Persona empleado, int total, int id) {
+		OrdenDeTrabajo nuevaOrden = new OrdenDeTrabajo(nombreProducto, diagnostico, cliente, empleado, total, id);
+		ordenesDeTrabajo.add(nuevaOrden);
+		if(this == cliente) {
+			empleado.ordenesDeTrabajo.add(nuevaOrden);
+		}
+		else {
+			cliente.ordenesDeTrabajo.add(nuevaOrden);
+		}
+		
+	}
+	
+	public void agregarNuevaOrden(String nombreProducto, String diagnostico, Persona empleado, int total, int id) {
+		OrdenDeTrabajo nuevaOrden = new OrdenDeTrabajo(nombreProducto, diagnostico, this, empleado, total, id);
+		ordenesDeTrabajo.add(nuevaOrden);
+		empleado.ordenesDeTrabajo.add(nuevaOrden);
+		
 	}
 	
 	public int getCantOrdenes() {
@@ -193,22 +175,33 @@ public class Persona {
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
-	/*public int getNumero() {
+	public String getNumero() {
 		return numero;
 	}
-	public void setNumero(int numero) {
+	
+	public void setNumero(String numero) {
 		this.numero = numero;
-	}*/
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = String.valueOf(numero);
+	}
 	
 	public int getRut() {
 		return rut;
 	}
+	
 	public void setRut(int rut) {
 		this.rut = rut;
+	}
+	
+	public void setRut(String rut) {
+		this.rut = Integer.parseInt(rut);
 	}
 	
 }
