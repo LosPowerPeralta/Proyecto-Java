1477cb666973c7a0d9a4d4f719f059859beb6ddd
JosephDonoso <86876275+JosephDonoso@users.noreply.github.com>
2022-09-02 20:31:51 -0400

    Estética y sobrecarga

--- ServicioTecnico.java
+++ ServicioTecnico.java
@@ -1,15 +1,10 @@
-import java.util.Hashtable;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class ServicioTecnico {
 
 	public static void main(String[] args) throws IOException {
-		Hashtable< Integer, Persona> clientes = new Hashtable< Integer, Persona>();
-		Hashtable< Integer, Persona> empleados = new Hashtable< Integer, Persona>();
-		ArrayList<Integer> rutsEmpleados = new ArrayList<Integer>();
 		
-		Menu menu = new Menu(clientes, empleados, rutsEmpleados);
+		Menu menu = new Menu();
 		menu.menuPrincipal();
 		
 	}
